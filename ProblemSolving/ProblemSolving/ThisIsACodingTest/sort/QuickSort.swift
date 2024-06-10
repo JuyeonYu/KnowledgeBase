27c30d8e698f96f2f929691a052448cf5b32f1bf
juyeon <remake867@gmail.com>
2024-06-11 00:21:05 +0900

    Sort - quick sort

---
+++ ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/QuickSort.swift
@@ -0,0 +1,19 @@
+//
+//  QuickSort.swift
+//  ProblemSolving
+//
+//  Created by  유 주연 on 6/10/24.
+//
+
+import Foundation
+
+// filter로 파티션을 나누게 되면 2n이 걸림. for문으로 left, right 변수를 사용하면 n번으로 끝낼 수 있음
+
+struct QuickSort {
+  func sort(list: [Int]) -> [Int] {
+    guard let pivot = list.first, list.count > 1 else { return list }
+    let left = list.filter { $0 < pivot }
+    let right = list.filter { $0 > pivot }
+    return sort(list: left) + [pivot] + sort(list: right)
+  }
+}
