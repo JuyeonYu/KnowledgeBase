515abc663309082aa9ab7c2ae1475b4d95912154
juyeon <remake111@medistaff.co.kr>
2024-06-11 16:59:48 +0900

    Sort - MergeSortedArray
    n + m 최적화 필요

---
+++ ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/MergeSortedArray.swift
@@ -0,0 +1,22 @@
+//
+//  MergeSortedArray.swift
+//  ProblemSolving
+//
+//  Created by  유 주연 on 6/11/24.
+//
+
+import Foundation
+
+/*
+ https://leetcode.com/problems/merge-sorted-array/
+ 
+ num1 앞에 m개와 num2앞 n개를 하나로 정렬한다.
+ */
+
+struct MergeSortedArray {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        nums1 = Array(nums1[0 ..< m])
+        nums1.append(contentsOf: nums2[0 ..< n])
+        nums1 = QuickSort().sort(list: nums1)
+    }
+}
