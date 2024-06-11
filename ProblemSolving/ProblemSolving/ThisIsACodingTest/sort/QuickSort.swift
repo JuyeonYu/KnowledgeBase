ff3eca421068603d44bc8b305fc3ba1c95b6b4dd
juyeon <remake111@medistaff.co.kr>
2024-06-11 10:28:32 +0900

    bug: quick sort 시 피봇과 중복값이 있을 경우 처리

--- ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/QuickSort.swift
+++ ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/QuickSort.swift
@@ -13,7 +13,8 @@ struct QuickSort {
   func sort(list: [Int]) -> [Int] {
     guard let pivot = list.first, list.count > 1 else { return list }
     let left = list.filter { $0 < pivot }
+    let middle = list.filter { $0 == pivot }
     let right = list.filter { $0 > pivot }
-    return sort(list: left) + [pivot] + sort(list: right)
+    return sort(list: left) + middle + sort(list: right)
   }
 }
