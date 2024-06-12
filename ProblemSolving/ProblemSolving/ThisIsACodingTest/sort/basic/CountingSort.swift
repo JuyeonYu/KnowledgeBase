bb22b12a624665f414d59ea3805c59c618bf118a
juyeon <remake867@gmail.com>
2024-06-12 09:56:03 +0900

    Sort - counting sort

--- ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/basic/CountingSort.swift
+++ ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/basic/CountingSort.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+/*
+ O(n + k)
+ 양수로만 이루어진 리스트이면서 최대값이 작을 때 유리함
+ */
+
+struct CountingSort {
+    func sort(_ list: [Int]) -> [Int] {
+        guard let max = list.max() else { return list }
+        var countingArray: [Int] = Array(repeating: 0, count: max + 1) // 값이 index에 직접 접근하기 위해 +1을 해줌
+        var sortedArray: [Int] = []
+        
+        for element in list {
+            countingArray[element] += 1
+        }
+        
+        for (index, element) in countingArray.enumerated() {
+            for _ in 0 ..< element {
+                sortedArray.append(index)
+            }
+        }
+        
+        
+        return sortedArray
+    }
+}
