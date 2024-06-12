805fe114fdd7e7c8c6524c07d371887677d72197
juyeon <remake867@gmail.com>
2024-06-12 09:09:06 +0900

    Sort - MergeSortedArray (m + n)

--- ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/MergeSortedArray.swift
+++ ProblemSolving/ProblemSolving/ThisIsACodingTest/sort/MergeSortedArray.swift
@@ -19,4 +19,28 @@ struct MergeSortedArray {
         nums1.append(contentsOf: nums2[0 ..< n])
         nums1 = QuickSort().sort(list: nums1)
     }
+    
+    func merge2(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var i = m - 1
+        var j = n - 1
+        var k = m + n - 1
+        
+        while i >= 0 && j >= 0 {
+            if nums1[i] > nums2[j] {
+                nums1[k] = nums1[i]
+                i -= 1
+            } else {
+                nums1[k] = nums2[j]
+                j -= 1
+            }
+            
+            k -= 1
+        }
+        
+        while j >= 0 {
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        }
+    }
 }
