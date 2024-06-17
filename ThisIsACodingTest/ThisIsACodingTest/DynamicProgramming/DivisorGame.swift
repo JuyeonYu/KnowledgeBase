21c2821b69d08648a150a025f07aff7c23e879ea
juyeon <remake867@gmail.com>
2024-06-17 09:52:44 +0900

    DP - divisor game

---
+++ ThisIsACodingTest/ThisIsACodingTest/DynamicProgramming/DivisorGame.swift
@@ -0,0 +1,19 @@
+//
+//  DevisorGame.swift
+//  KnowledgeBase
+//
+//  Created by  유 주연 on 6/17/24.
+//
+
+/*
+ https://leetcode.com/problems/divisor-game/description/
+ DP로 분류되어있으나 굳이 DP로 안풀어도 됨
+ */
+
+import Foundation
+
+struct DivisorGame {
+    func solution(_ n: Int) -> Bool {
+        n % 2 == 0
+    }
+}
